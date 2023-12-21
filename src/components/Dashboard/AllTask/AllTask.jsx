b54798052c01a93a81304fef8367eee68d927438
sakibhossain22 <sakib01181@gmail.com>
2023-12-22 00:46:00 +0600

    drag and drop added

--- src/components/Dashboard/AllTask/AllTask.jsx
+++ src/components/Dashboard/AllTask/AllTask.jsx
@@ -1,15 +1,17 @@
-import React, { useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
+import useTasks from '../../useTasks/useTasks';
+import { AuthContext } from '../../AuthProvider/AuthProvider';
 
 const AllTask = () => {
-  const [todos, setTodos] = useState([
-    { id: 1, name: 'mango', status: 'todo' },
-    { id: 2, name: 'apple', status: 'todo' },
-    { id: 3, name: 'orange', status: 'todo' },
-    { id: 4, name: 'cucumber', status: 'todo' },
-  ]);
-
+  const { data, refetch, isLoading, isError } = useTasks()
+  console.log(data)
+  const { user, loading } = useContext(AuthContext)
+  const [todos, setTodos] = useState(data);
   const [completedTodos, setCompletedTodos] = useState([]);
-
+  const [ongoingTodos, setOngoingTodos] = useState([]);
+useEffect(()=>{
+  setTodos(data)
+},[data])
   const handleDragStart = (e, id) => {
     e.dataTransfer.setData('text/plain', id);
   };
@@ -21,61 +23,132 @@ const AllTask = () => {
   const handleDrop = (e, target) => {
     e.preventDefault();
     const todoId = e.dataTransfer.getData('text/plain');
-    const draggedTodo = todos.find((item) => item.id === parseInt(todoId, 10));
-    const completedDraggedTodo = completedTodos.find((item) => item.id === parseInt(todoId, 10));
+    const draggedTodo = todos.find((item) => item?.id === parseInt(todoId, 10));
+    const completedDraggedTodo = completedTodos.find((item) => item?.id === parseInt(todoId, 10));
+    const ongoingDraggedTodo = ongoingTodos.find((item) => item?.id === parseInt(todoId, 10));
 
     if (draggedTodo) {
-      // Moving from Todo to Complete
-      setCompletedTodos((prev) => [...prev, { ...draggedTodo, status: 'complete' }]);
+      if (target === 'complete') {
+        // Move from Todo to Complete
+        setCompletedTodos((prev) => [...prev, { ...draggedTodo, status: 'complete' }]);
+      } else if (target === 'ongoing') {
+        // Move from Todo to Ongoing
+        setOngoingTodos((prev) => [...prev, { ...draggedTodo, status: 'ongoing' }]);
+      }
+
       setTodos((prev) => prev.filter((item) => item.id !== draggedTodo.id));
     } else if (completedDraggedTodo) {
-      // Moving from Complete to Todo
-      setTodos((prev) => [...prev, { ...completedDraggedTodo, status: 'todo' }]);
+      if (target === 'todo') {
+        // Move from Complete to Todo
+        setTodos((prev) => [...prev, { ...completedDraggedTodo, status: 'todo' }]);
+      } else if (target === 'ongoing') {
+        // Move from Complete to Ongoing
+        setOngoingTodos((prev) => [...prev, { ...completedDraggedTodo, status: 'ongoing' }]);
+      }
+
       setCompletedTodos((prev) => prev.filter((item) => item.id !== completedDraggedTodo.id));
+    } else if (ongoingDraggedTodo) {
+      if (target === 'todo') {
+        // Move from Ongoing to Todo
+        setTodos((prev) => [...prev, { ...ongoingDraggedTodo, status: 'todo' }]);
+      } else if (target === 'complete') {
+        // Move from Ongoing to Complete
+        setCompletedTodos((prev) => [...prev, { ...ongoingDraggedTodo, status: 'complete' }]);
+      }
+
+      setOngoingTodos((prev) => prev.filter((item) => item.id !== ongoingDraggedTodo.id));
     }
   };
-
+  if (loading) return <div className='flex items-center justify-center h-screen'><span className="loading loading-spinner loading-lg"></span></div>
   return (
-    <div className="flex">
-      <div className="w-1/2 p-4">
-        <h2 className="text-lg font-bold mb-4">Todo List</h2>
+    <div className='mr-3 mt-2'>
+      <div className="">
+        <h2 className="text-xl border-l-4 border-red-500 pl-2 uppercase  font-bold mb-2">Todo List</h2>
         <div
-          className="border border-dashed p-4 h-full"
+          className="py-4 pb-5 h-full"
           onDragOver={(e) => handleDragOver(e)}
           onDrop={(e) => handleDrop(e, 'todo')}
         >
-          {todos.map((todo) => (
+          <div className='grid grid-cols-12 text-white font-bold bg-yellow-500 py-2 rounded mb-2 items-center justify-between px-4'>
+            <span className='col-span-5'>Title</span>
+            <span className='col-span-5'>Priority</span>
+            <span className='col-span-2'>Deadline</span>
+          </div>
+          {todos?.map((todo) => (
             <div
               key={todo.id}
               draggable
               onDragStart={(e) => handleDragStart(e, todo.id)}
-              className="cursor-pointer bg-gray-200 p-2 mb-2"
+              className="cursor-pointer bg-yellow-200 p-2 mb-2"
             >
-              {todo.name}
+              <div className='grid grid-cols-12  items-center justify-between px-4'>
+                <span className='col-span-5'>{todo.title}</span>
+                <span className='col-span-5 '>{todo.priority}</span>
+                <span className='col-span-2 '>{todo.deadline}</span>
+              </div>
             </div>
           ))}
         </div>
       </div>
-
-      <div className="w-1/2 p-4">
-        <h2 className="text-lg font-bold mb-4">Completed</h2>
+      <div className="">
+        <h2 className="text-xl border-l-4 border-red-500 pl-2 uppercase  font-bold mb-4">Ongoing</h2>
         <div
-          className="border border-dashed p-4 h-full"
+          className="pb-10 h-full"
+          onDragOver={(e) => handleDragOver(e)}
+          onDrop={(e) => handleDrop(e, 'ongoing')}
+        >
+           <div className='grid grid-cols-12 text-white font-bold bg-blue-500 py-2 rounded mb-2 items-center justify-between px-4'>
+            <span className='col-span-5'>Title</span>
+            <span className='col-span-5'>Priority</span>
+            <span className='col-span-2'>Deadline</span>
+          </div>
+          {ongoingTodos?.map((todo) => (
+            <div
+              key={todo.id}
+              draggable
+              onDragStart={(e) => handleDragStart(e, todo.id)}
+              className="cursor-pointer bg-blue-200 p-2 mb-2"
+            >
+              <div className='grid grid-cols-12  items-center justify-between px-4'>
+                <span className='col-span-5'>{todo.title}</span>
+                <span className='col-span-5 '>{todo.priority}</span>
+                <span className='col-span-2 '>{todo.deadline}</span>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="">
+        <h2 className="text-xl border-l-4 border-red-500 pl-2 uppercase  font-bold mb-4">Completed</h2>
+        <div
+          className="pb-10 h-full"
           onDragOver={(e) => handleDragOver(e)}
           onDrop={(e) => handleDrop(e, 'complete')}
         >
-          {completedTodos.map((todo) => (
+           <div className='grid grid-cols-12 text-white font-bold bg-green-500 py-2 rounded mb-2 items-center justify-between px-4'>
+            <span className='col-span-5'>Title</span>
+            <span className='col-span-5'>Priority</span>
+            <span className='col-span-2'>Deadline</span>
+          </div>
+          {completedTodos?.map((todo) => (
             <div
               key={todo.id}
               draggable
               onDragStart={(e) => handleDragStart(e, todo.id)}
               className="cursor-pointer bg-green-200 p-2 mb-2"
             >
-              {todo.name}
+              <div className='grid grid-cols-12  items-center justify-between px-4'>
+                <span className='col-span-5'>{todo.title}</span>
+                <span className='col-span-5 '>{todo.priority}</span>
+                <span className='col-span-2 '>{todo.deadline}</span>
+              </div>
+
             </div>
           ))}
         </div>
       </div>
+
+
     </div>
   );
 };
